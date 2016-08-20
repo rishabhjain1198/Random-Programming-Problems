c8d2ac357ec2457a179b658c51a42a8ecebdfaa8
Rishabh <rishabh.jain1198@gmail.com>
2016-08-20 21:21:06 +0530

    Routine commit

--- inParen.cpp
+++ inParen.cpp
@@ -1,13 +1,97 @@
 #include<iostream>
 #include<vector>
+#include<cstring>
 
 using namespace std;
 
-vector<string> removeInvalidParenthesis(string s)
+void traverseit(int right, int left, int pair, string &stackit, vector<string> &finalResult, int pos, string &s)
 {
-	vector<string> finalResult;
-	finalResult.push_back(s);
+	cout<<" YOYO FUNC EXE"<<endl<<s.length()<<" "<<pos<<endl;
+	cout<<"LEFT: "<<left<<" RIGHT: "<<right<<" PAIR: "<<pair<<" POS: "<<pos<<endl;
+	//check if the recursion is at the end of its life
+	if(pos >= s.length())
+	{
+		if(!left && !right && !pair)
+		{
+			//checking for duplicates
+			int j = 1;
+			for(int i=0; i<finalResult.size(); i++)
+			{
+				if(!stackit.compare(finalResult[i]))
+					j = 0;
+			}
+			
+			if(j)
+				{finalResult.push_back(stackit); cout<<"donezo"<<endl;}
+		}
+	}
+	//time to define the recursion
+	else
+	{
+		if(s[pos] == '(')
+		{
+			if(left)
+				{traverseit(right, --left, pair, stackit, finalResult, ++pos, s);}
+			stackit += '(';
+			cout<<" ADDED TOT EH FKIN STACK M8"<<endl;
+			traverseit(right, left, ++pair, stackit, finalResult, ++pos, s);
+		}
+		
+		else if(s[pos] == ')')
+		{
+			if(right)
+				{traverseit(--right, left, pair, stackit, finalResult, ++pos, s);}
+			stackit += ')';
+			if(pair)
+				{traverseit(right, left, --pair, stackit, finalResult, ++pos, s);}
+		}
+		else
+		{
+			stackit += s[pos];
+			traverseit(right, left, pair, stackit, finalResult, ++pos, s);
+		}
+	}
+}
 
+vector<string> removeInvalidParenthesis(string s)
+{	
+	//Initialize the variables
+	vector<string> finalResult;
+	string stackit;
+	int left = 0, right = 0, pair = 0, pos = 0;
+
+	//Scann every single character of the string
+	for(int i = 0; i<s.length(); i++)
+	{
+		if(s[i] == '(')
+		{
+			left++;
+			cout<<"LEFT INCREMENETED"<<endl;
+		}
+		
+		else if(s[i] == ')')
+		{
+			if(left)
+			{
+				left--;
+				cout<<"LEFT DECRE"<<endl;
+			}
+			else
+			{
+				right++;
+				cout<<"RIGHT INCRE"<<endl;
+			}
+		}
+	}
+	//Scan of string complete, now to call traversal of the string for DFS
+	
+	traverseit(left, right, pair, stackit, finalResult, pos, s);	
+	
+	cout<<endl;
+	for(int i = 0; i<finalResult.size(); i++)
+	{
+		cout<<finalResult[i]<<endl;
+	}	
 	return finalResult;
 
 }
@@ -15,6 +99,6 @@ vector<string> removeInvalidParenthesis(string s)
 
 int main()
 {
-	cout<<removeInvalidParenthesis("rekt")[0]<<endl;
+	removeInvalidParenthesis("()())()");
 	return 0;
 }
