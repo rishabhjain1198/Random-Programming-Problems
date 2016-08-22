bf9184f5c6b1b0ca95218a919644a15950f76447
Rishabh <rishabh.jain1198@gmail.com>
2016-08-23 01:09:03 +0530

    Routine commit

--- rusDoll.cpp
+++ rusDoll.cpp
@@ -3,51 +3,38 @@
 
 using namespace std;
 
-void mergeIt(vector<pair<int, int> > &envelopes, int low, int mid, int high, vector<pair<int, int> > &dpit)
+void printit(vector<pair<int, int> > &envelopes)
 {
-    int part1, part2, i;
-    for(part1 = low, part2 = mid+1, i = 0; part1 <= mid && part2 <= high && i <= high; i++)
+    for(int i = 0; i<envelopes.size(); i++)
     {
-        if(envelopes[part1].first > envelopes[part2].first)
-        {
-                dpit.push_back(envelopes[part1++]);
-        }
-        else if(envelopes[part1].first <= envelopes[part2].first)
-        {
-                dpit.push_back(envelopes[part2++]);
-        }
+        cout<<envelopes[i].first<<" "<<envelopes[i].second<<endl;
     }
-    while(part1<=mid)
-    {
-        dpit.push_back(envelopes[part1++]);
-    }
-    while(part2<=high)
-    {
-        dpit.push_back(envelopes[part2++]);
-    }
-
-    while(low<=high)
-    {
-        envelopes[low] = dpit[low];
-        low++;
-    }
+    cout<<endl;
 }
 
-void mergeSort(vector<pair<int, int> > &envelopes, int low, int high, vector<pair<int, int> > &dpit)
+void firstSort(vector<pair<int, int> > &envelopes)
 {
-   if(low<high)
-   {
-        int mid = (low + high) / 2;
-        mergeSort(envelopes, low, mid, dpit);
-        mergeSort(envelopes, mid+1, high, dpit);
-        mergeIt(envelopes, low, mid, high, dpit);
-   }
-    
+    bool swapped = true;
+    int j = envelopes.size();
+    while(swapped)
+    {
+        j--;
+        swapped = false;
+        for(int i = 0; i < j; i++)
+        {
+            if(envelopes[i].first<envelopes[i+1].first)
+            {
+                int number1 = envelopes[i].first; int number2 = envelopes[i].second;
+                envelopes[i] = envelopes[i+1];
+                envelopes[i+1] = make_pair(number1, number2);
+                swapped = true;
+            }
+        }
+    }
 }
-
 void secondSort(vector<pair<int, int> > &envelopes, int start)
 {
-    if(start!=(envelopes.size())-1)
+    if(start<(envelopes.size())-1)
     {
         int endi = start;
         for(int i = start; i<envelopes.size(); i++)
@@ -57,18 +44,22 @@ void secondSort(vector<pair<int, int> > &envelopes, int start)
                 endi = i;
            }
         }
-        
-        for(int i = start; i<=endi; i++)
+        bool swapped = true;
+        int j = endi+1;
+       while(swapped)
         {
-            for(int j = start; j<=i; j++)
-            {
-                if(envelopes[j].second > envelopes[i].second)
+            j--;
+            swapped = false;
+        for(int i = start; i<j; i++)
+        {
+                if(envelopes[i].second < envelopes[i+1].second)
                 {
-                    pair<int, int> temp  = envelopes[j];
-                    envelopes[j]= envelopes[i];
-                    envelopes[i]= temp;
+                int number1 = envelopes[i].first; int number2 = envelopes[i].second;
+                envelopes[i] = envelopes[i+1];
+                envelopes[i+1] = make_pair(number1, number2);
+                swapped = true;
                 }
-            }
+        }
         }
 
         secondSort(envelopes, endi+1);
@@ -93,49 +84,46 @@ bool searchit(vector<pair<int, int> > &envelopes, vector<pair<int, int> > &dpit,
 void traverseit(vector<pair<int, int> > &envelopes, int pos, vector<pair<int, int> > dpit, int &finalAnswer, int tempCount)
 {
     if(pos!=envelopes.size()-1){
-    if(!searchit(envelopes, dpit, pos))
-    {
-        dpit.push_back(envelopes[pos]);
         for(int i = pos+1; i<envelopes.size(); i++)
         {
             if(envelopes[pos].first > envelopes[i].first && envelopes[pos].second > envelopes[i].second)
             {
                traverseit(envelopes, i, dpit, finalAnswer, tempCount+1);
-               break;
             }
         }
-    }}
-    else
-    {
-       if(tempCount>finalAnswer)
-            finalAnswer = tempCount;
     }
+    if(tempCount>finalAnswer)
+       finalAnswer = tempCount;
 }
 
 int maxEnvelopes(vector<pair<int, int> > &envelopes)
 {
-    int finalAnswer;
-    vector<pair<int, int> > dpit;     
-    mergeSort(envelopes, 0, envelopes.size(), dpit);
-    dpit.clear();
-    secondSort(envelopes, 0);
-    for(int i = 0; i<envelopes.size(); i++)
+    int finalAnswer = 0;
+    if(envelopes.size()!=0)
     {
-        traverseit(envelopes, i, dpit, finalAnswer, 0);
+        vector<pair<int, int> > dpit;     
+        firstSort(envelopes);
+        secondSort(envelopes, 0);
+        for(int i = 0; i<envelopes.size(); i++)
+        {   
+            traverseit(envelopes, i, dpit, finalAnswer, 1);
+        }
     }
-    return finalAnswer; 
+    
+    cout<<finalAnswer<<endl;
+    return finalAnswer;
 }
 
 int main()
 {
     vector<pair<int, int> > envelopes;
-    envelopes.push_back(make_pair(5,4));
-    envelopes.push_back(make_pair(6,4));
-    envelopes.push_back(make_pair(6,7));
-    envelopes.push_back(make_pair(2,3));
-    
+    envelopes.push_back(make_pair(46,89));
+    envelopes.push_back(make_pair(50,52));
+envelopes.push_back(make_pair(52,68));
+envelopes.push_back(make_pair(72,45));
+envelopes.push_back(make_pair(77,81));
 
     maxEnvelopes(envelopes);
-
+    
     return 0;
 }
