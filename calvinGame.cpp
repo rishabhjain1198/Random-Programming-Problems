f9eba069ee45ef9fa9967ab620493d384798618e
Rishabh <rishabh.jain1198@gmail.com>
2016-08-25 02:19:14 +0530

    Routine commit

--- calvinGame.cpp
+++ calvinGame.cpp
@@ -24,18 +24,19 @@ void reverseTraverseSquares(long long int Squares [], long long int sizeSquares,
 
 }
 
-
-void traverseSquares(long long int Squares [], long long int sizeSquares, long long int maxy [], long long int pos, long long int currentValue)
+void newTraverseSquares(long long int Squares[], long long int sizeSquares, long long int maxy [], long long int bmaxy [], long long int pos, long long int k)
 {
     if(pos>=sizeSquares)
         return ;
-    if(currentValue+Squares[pos] < maxy[pos])
+    if(pos<=k)
+    {
+        maxy[pos] = 0;
+        newTraverseSquares(Squares, sizeSquares, maxy, bmaxy, pos+1, k);
         return ;
+    }
 
-    currentValue += Squares[pos];
-    maxy[pos] = currentValue;
-    traverseSquares(Squares, sizeSquares, maxy, pos+1, currentValue);
-    traverseSquares(Squares, sizeSquares, maxy, pos+2, currentValue);
+    maxy[pos] = Squares[pos] + bmaxy[pos] - bmaxy[k] - Squares[k];
+    newTraverseSquares(Squares, sizeSquares, maxy, bmaxy, pos+1, k);
 }
 
 int main()
@@ -47,19 +48,13 @@ int main()
     long long int Squares [sizeSquares], maxy [sizeSquares], bmaxy[sizeSquares];
     for(long long int i = 0; i<sizeSquares; i++)
     {
-        if(i>k)
-            maxy[i] = -99999999999999;
-        else
-            maxy[i] = 0;
         bmaxy[i] = -999999999999999;
         cin>>Squares[i];
     }
-    //Input taken successfully and every element of maxy set to -9999
-    //Updating maxy by traversing squares and implementing dp using maxy itself
-    traverseSquares(Squares, sizeSquares,  maxy, k+1, 0);
-    traverseSquares(Squares, sizeSquares,  maxy, k+2, 0);
 
     reverseTraverseSquares(Squares, sizeSquares, bmaxy, 0, 0, 0);
+
+    newTraverseSquares(Squares, sizeSquares, maxy, bmaxy, 0, k);
     
     long long int maxx = 0;
     for(long long int i = 0; i<sizeSquares; i++)
