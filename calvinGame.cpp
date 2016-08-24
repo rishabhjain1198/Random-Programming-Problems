801ab6f9f13c555cf16bdb5a5efebe0ae12c4610
Rishabh <rishabh.jain1198@gmail.com>
2016-08-24 20:47:08 +0530

    Routine commit

--- calvinGame.cpp
+++ calvinGame.cpp
@@ -4,53 +4,37 @@ using namespace std;
 
 void printit(int array [], int size)
 {
-	cout<<endl<<"ARRAY IS  ";
 	for(int i =0; i<size; i++)
 	{
 		cout<<array[i]<<" ";
 	}
-	cout<<"    YA DUN"<<endl;
 }
+
+void reverseTraverseSquares(int Squares [], int sizeSquares, int bmaxy [], int pos, int currentValue, int prevValue)
+{
+    if(pos>=sizeSquares)
+        return ;
+    if(currentValue+prevValue < bmaxy[pos])
+        return ;
+
+    currentValue += prevValue;
+    bmaxy[pos] = currentValue;
+    reverseTraverseSquares(Squares, sizeSquares, bmaxy, pos+1, currentValue, Squares[pos]);
+    reverseTraverseSquares(Squares, sizeSquares, bmaxy, pos+2, currentValue, Squares[pos]);
+
+}
+
 
 void traverseSquares(int Squares [], int sizeSquares, int maxy [], int pos, int currentValue)
 {
     if(pos>=sizeSquares)
         return ;
-
-    if(currentValue+Squares[pos] <= maxy[pos])
+    if(currentValue+Squares[pos] < maxy[pos])
         return ;
 
-    printit(maxy, sizeSquares);
-    currentValue += Squares[pos];
-    maxy[pos] = currentValue;
-    traverseSquares(Squares, sizeSquares, maxy, pos+1, currentValue);
-    traverseSquares(Squares, sizeSquares, maxy, pos+2, currentValue);
-}
-
-int findMax(int maxy [], int sizeSquares, int &j)
-{
-   int maxx = -9999;
-   for(int i = 0; i<sizeSquares; i++)
-   {
-        if(maxy[i] > maxx)
-        {maxx = maxy[i];
-            j = i;}
-   }
-   return maxx;
-}
-
-void reverseTraverseSquares(int Squares [], int sizeSquares, int maxy [], int pos, int currentValue)
-{
-    if(pos<0)
-        return ;
-
-    if(currentValue+Squares[pos] <= maxy[pos])
-        return ;
-
-    currentValue += Squares[pos];
-    maxy[pos] = currentValue;
-    reverseTraverseSquares(Squares, sizeSquares, maxy, pos-1, currentValue);
-    reverseTraverseSquares(Squares, sizeSquares, maxy, pos-2, currentValue);
+    maxy[pos] = currentValue + Squares[pos];
+    traverseSquares(Squares, sizeSquares, maxy, pos+1, currentValue + Squares[pos]);
+    traverseSquares(Squares, sizeSquares, maxy, pos+2, currentValue + Squares[pos]);
 }
 
 
@@ -64,25 +48,25 @@ int main()
     int Squares [sizeSquares], maxy [sizeSquares], bmaxy[sizeSquares];
     for(int i = 0; i<sizeSquares; i++)
     {
-        maxy[i] = -9999;
-        bmaxy[i] = -9999;
+        maxy[i] = -999999999 ? i>k : maxy[i] = 0;
+        bmaxy[i] = -999999999;
         cin>>Squares[i];
     }
     //Input taken successfully and every element of maxy set to -9999
+    bmaxy[0] = 0;
 
     //Updating maxy by traversing squares and implementing dp using maxy itself
     traverseSquares(Squares, sizeSquares,  maxy, k+1, 0);
-    traverseSquares(Squares, sizeSquares, maxy, k+2, 0);
+    traverseSquares(Squares, sizeSquares,  maxy, k+2, 0);
 
-    int j;
-    int maxx1 = findMax(maxy, sizeSquares, j);
-    //start reverse traversal of update of maxy
-    if(maxx1<=0)
-	j=k;
-    reverseTraverseSquares(Squares, sizeSquares, bmaxy, j-1, 0);
-    reverseTraverseSquares(Squares, sizeSquares, bmaxy, j-2, 0);
+    reverseTraverseSquares(Squares, sizeSquares, bmaxy, 0, 0, 0);
+    
+    int maxx = 0;
+    for(int i = 0; i<sizeSquares; i++)
+    {
+        maxx = max(maxx, maxy[i] + bmaxy[i]);
+    }
 
-
-    cout<<maxx1+bmaxy[0];
+    cout<<maxx;
     return 0;
 }
